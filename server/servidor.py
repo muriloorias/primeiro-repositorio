6d19b725a1d242c3bf83274e380b4ab75c464131
murilo orias <mu.a.o@outlook.com>
2024-01-17 22:25:43 -0300

    primeiro commit

---
+++ server/servidor.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("inicial.html")
+
+
+@app.route("/anonimo")
+def anonimo():
+    return render_template("pagina.html")
+
+
+
+
+@app.route("/calculadora")
+def calculadora():
+    return render_template("calculator.html")
+
+
+
+
+   
+if __name__ == "__main__":
+    app.run(debug=True)
